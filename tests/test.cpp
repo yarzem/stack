e601b15e96e54a8ce20f2e7339fe6b1744aa3c0d
Sergei2000 <dsdem@mail.ru>
2020-02-12 23:15:29 +0300

    final hat trick kozhaniy myach Gena Bukin

--- tests/test.cpp
+++ tests/test.cpp
@@ -7,13 +7,12 @@ TEST(Example, EmptyTest) {
     dog Pitbul(2);
     stack<dog> st;
     st.push(Lavr);
-    EXPECT_EQ(st.head().i,1);
+    EXPECT_EQ(st.head().i, 1);
     st.push(Pitbul);
-    EXPECT_EQ(st.head().i,2);
+    EXPECT_EQ(st.head().i, 2);
     st.push_emplace(3);
-    EXPECT_EQ(st.head().i,3);
-    EXPECT_EQ(st.head().word,"gav");
-    
+    EXPECT_EQ(st.head().i, 3);
+    EXPECT_EQ(st.head().word, "gav");
 }
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
