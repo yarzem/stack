d05f19211e1a62609d105f134b27988aa72f95f9
Sergei2000 <dsdem@mail.ru>
2020-02-12 23:05:34 +0300

    final hat trick kozhaniy myach

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,21 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
-
+#include "source.cpp"
 TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+     dog Lavr(1);
+    dog Pitbul(2);
+    stack<dog> st;
+    st.push(Lavr);
+    EXPECT_EQ(st.head().i,1);
+    st.push(Pitbul);
+    EXPECT_EQ(st.head().i,2);
+    st.push_emplace(3);
+    EXPECT_EQ(st.head().i,3);
+    EXPECT_EQ(st.head().word,"gav");
+    
+}
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
