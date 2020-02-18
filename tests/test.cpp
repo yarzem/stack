81c7cb208cf06f985d04d96efa3575a27ad78497
Sergei2000 <dsdem@mail.ru>
2020-02-19 00:37:32 +0300

    8

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,7 +3,29 @@
 #include <gtest/gtest.h>
 #include "header.hpp"
 TEST(Example, Stack) {
-   
+    EXPECT_EQ(std::is_move_constructible<stack1<int>>::value, true);
+    EXPECT_EQ(std::is_move_assignable<stack1<int>>::value, true);
+    EXPECT_EQ(std::is_move_constructible<stack2<int>>::value, true);
+    EXPECT_EQ(std::is_move_assignable<stack2<int>>::value, true);
+    EXPECT_EQ(std::is_copy_constructible<stack1<int>>::value, true);
+    EXPECT_EQ(std::is_copy_assignable<stack1<int>>::value, true);
+    EXPECT_EQ(std::is_copy_constructible<stack2<int>>::value, true);
+    EXPECT_EQ(std::is_copy_assignable<stack2<int>>::value, true);
+    stack1 <int> s;
+    int a = 48;
+    s.push(a);
+    EXPECT_EQ(s.head(), 48);
+    stack2 <std::string> x;
+    x.push("kek");
+    x.push("lol");
+    x.push("azaz");
+    EXPECT_EQ((std::string) x.pop(), "azaz");
+    dog Lavr;
+    stack<dog> st;
+    st.push(Lavr);
+    st.push_emplace("world");
+    EXPECT_EQ(st.head().word, "world");
+    
 }
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
